0eb92187ab873904c764f68701da68cecc753177
Saikrishna1504 <saikrishna26918@gmail.com>
2024-03-04 14:19:03 +0000

    selene: Enable FBEv2
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>
    Signed-off-by: Hiura Mihate <tillyourteargoes@murena.io>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -95,6 +95,7 @@ TARGET_BOARD_PLATFORM := mt6768
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Recovery
 TARGET_USERIMAGES_USE_EXT4 := true
